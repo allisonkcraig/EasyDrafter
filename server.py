42cfbec4ca6e28f277b85cc30eaae7016c5923be
Allison  Craig <allison@craig.net>
2015-07-31 16:10:44 -0700

    add log in function and template

--- server.py
+++ server.py
@@ -1,7 +1,15 @@
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, redirect, flash, session
 import os
+from flask_debugtoolbar import DebugToolbarExtension
+import jinja2
+import secrets #check that this is correct
+import model
 
 app = Flask(__name__)
+
+app.secret_key = secrets.secret_key #check that this is correct
+
+app.jinja_env.undefined = jinja2.StrictUndefined
 
 @app.route('/')
 def home_page():
@@ -21,6 +29,44 @@ def measure_page():
 def patter_page():
 	
 	return render_template("canvas.html")
+
+
+@app.route("/login", methods=["GET"])
+def show_login():
+    """Show login form."""
+
+    return render_template("login.html")
+
+
+@app.route("/login", methods=["POST"])
+def process_login():
+    """Log user into site.
+    Find the user's login credentials located in the 'request.form'
+    dictionary, look up the user, and store them in the session.
+    """
+
+    email_input = request.form.get("email")
+    pword_input = request.form.get("password")
+
+    customer = model.User.get_by_email(email_input)
+    if customer is None:
+        flash("No such email")
+        return redirect("/login")
+    else:
+        if pword_input != customer.pword:
+            flash("Incorrect password")
+            return redirect("/login")
+        else:
+            flash("Login successful!!")
+            session['logged_in_customer_email'] = email_input
+            return redirect("/")
+
+
+@app.route("/logout")
+def process_logout():
+    del session['user_id']
+    flash("You have been logged out")
+    return redirect("/")
 
 
 if __name__ == "__main__":
