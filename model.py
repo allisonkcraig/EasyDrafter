c1133f9dfe44f3fa892aa1cdb92a475774c87589
Allison  Craig <allison@craig.net>
2015-07-31 15:59:55 -0700

    delete libraries unused

--- model.py
+++ model.py
@@ -1,8 +1,7 @@
 """Models and database functions for Ratings project."""
 
 from flask_sqlalchemy import SQLAlchemy
-from correlation import pearson
-import math
+
 # This is the connection to the SQLite database; we're getting this through
 # the Flask-SQLAlchemy helper library. On this, we can find the `session`
 # object, where we do most of our interactions (like committing, etc.)
